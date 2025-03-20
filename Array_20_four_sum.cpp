e7c348c8a88e2ceac0666265568ec382eaa39ac1
Shailja <genieshailja@gmail.com>
2025-03-21 00:09:20 +0530

    Update Array_20_four_sum.cpp

--- Array_20_four_sum.cpp
+++ Array_20_four_sum.cpp
@@ -6,31 +6,29 @@ public:
         vector<vector<int>>ans;
         int n=nums.size();
         sort(nums.begin(),nums.end());
+       
         for(int i=0;i<n;i++){
              if(i!=0 && nums[i]==nums[i-1] )continue;
             for(int j=i+1;j<n;j++){
-                 if(j>1 && nums[j-1]==nums[j])continue;
-                int tar=target-(nums[i]+nums[j]);
+                 if(j>i+1 && nums[j-1]==nums[j])continue;
+                
                 int k=j+1;
                 int h=n-1;
-                while(k<h){
-                int summ=nums[k]+nums[h];
-                if(summ==tar){
+                 while(k<h){
+                    long long summ=(long long)nums[i]+nums[j]+nums[k]+nums[h];
+                    
+                   if(summ==target){
                     ans.push_back({nums[i],nums[j],nums[k],nums[h]});
                     k++;h--;
+                    while(k<h && nums[k]==nums[k-1])k++;
+                    while(k<h && nums[h]==nums[h+1])h--;
                 }
-                else if(summ>tar){
-                    h--;
-                    while(h>0 && nums[h]==nums[h+1]){
-                        h--;
-                    }
+                else if(summ>target){
+                    h--;     
                 }
                 else{
                     //sum<tar
                      k++;
-                     while(k<h && nums[k]==nums[k-1]){
-                        k++;
-                     }
                           }
                 }
             }
